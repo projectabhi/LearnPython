09bd775340045b3d855ee741d67762436accab62
borokali <projectabhi@gmail.com>
2020-09-01 20:40:06 +0530

    classmethod and static method

--- basic/ClassAndObject/ClassConstructor.py
+++ basic/ClassAndObject/ClassConstructor.py
@@ -1,14 +1,34 @@
 class ComplexNumbers:
-    def __init__(self, r = 0, i = 0):
+
+    #default constructor
+    def __init__(self, r=0, i=0):
         self.real = r
         self.imag = i
 
+    #regular method using instance as argument
     def getdata(self):
-        print("{0} + {1}j".format(self.real, self.imag))
+        print("{0} + {1}".format(self.real, self.imag))
 
-c1 = ComplexNumbers(2, 3)
+    # additional constructor
+    @classmethod
+    def from_str(cls,numberStr):
+        real,imag,attr=numberStr.split('-')
+        return cls(real,imag)
 
+    # static method not using instance as argument
+    @staticmethod
+    def id_realnumber(number):
+        if number%10 != 0:
+            return False
+        return True
+
+c1 = ComplexNumbers(2,3)
 c1.getdata()
+
+c1 = ComplexNumbers.from_str('5-9-8')
+c1.getdata()
+
+print(c1.id_realnumber(10.05))
 
 c2 = ComplexNumbers(5)
 c2.attr = 10
--- basic/ClassAndObject/SimpleClass.py
+++ basic/ClassAndObject/SimpleClass.py
@@ -1,12 +1,15 @@
 class SimpleClass:
     "This is my class"
     a = 10
+    def __init__(self):
+        self.a=11
+
     def func(self):
         print("Hello")
 
-
+s=SimpleClass()
 print(SimpleClass.a)
-
+print(s.a)
 print(SimpleClass.func)
 
 SimpleClass.func(' ')
